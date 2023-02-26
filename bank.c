9d47dbc0b96e804ca11db6119780c4312615ffa2
vincnx <vincent.celcio@gmail.com>
2023-02-27 04:15:38 +0700

    Revert "menambahkan panah di menu regis & login"
    This reverts commit e10c659a577f411942f413cb4fcc9ffe7f85d004.

--- bank.c
+++ bank.c
@@ -2,199 +2,123 @@
 #include <conio.h>
 #include <windows.h>
 
-// bagian 1 (register & login)
-int main_1();
-int menu_1();
-int menu_1_1();
-int menu_1_2();
-// end bagian 1
-// bagian 2
+void login();
 
-// tools
-void gotoxy();
-void get_password();
+struct datauser
+{
+    char pw1, uname1[100];
+    char password1[100];
 
-// struct & global variable
-struct data_login{
-    char nama[100];
-    char password[8];
-};
-struct data_login data_login[100];
+    char pw2, uname2[100];
+    char password2[100];
+} datauser;
 
-int idx_data_login = 0; // index lokasi data_login
-int login_cond = 0; // kondisi login (1 = sudah login) (0 = belum login)
-int coba = 2; // maks percobaan login
-//
+int coba = 2;
 
-int main(){
-    system("COLOR f1");
-    if (!login_cond){
-        main_1();
-    }
+void regis()
+{
+    char username[100];
+    char pw[100];
+    int i = 0, x = 10;
+
     system("cls");
-    printf("bisa login!!");
-}
+    printf("Sign Up \n\n");
+    printf("Username : ");
+    scanf(" %[^\n]s", &datauser.uname1);
+    printf("Password : ");
 
-// bagian 1 (register & login)
-int main_1(){ // main register & login
-    int pilihan = menu_1();
-    switch (pilihan){
-    case 1: // registrasi
-        menu_1_1();
-        main_1();
-        break;
-    case 2: // login
-        if (idx_data_login){
-            menu_1_2();
+    while ((datauser.pw1 = (char)_getch()) != '\r')
+    {
+        if (datauser.pw1 == 8)
+        {
+            datauser.password1[i] = '\0';
+            printf(" ");
+            i--;
+            x--;
         }
-        else{
-            system("cls");
-            printf("Harap Registrasi Terlebih Dahulu.");
-            Sleep(1000);
-            main_1();
+        else
+        {
+            datauser.password1[i] = datauser.pw1;
+            printf("*");
+            x++;
+            i++;
         }
-        break;
     }
+
+    printf("\n");
+
+    system("cls");
+    printf("Pendaftaran berhasil!");
+    getch();
+    system("cls");
+    login();
 }
 
-int menu_1(){ // pilihan menu regis & login
-    int pos = 3;
-    while(1){
+void login()
+{
+    char username[100];
+    char pw[100];
+    int j = 0, y = 10;
+
+    system("cls");
+    printf("LOGIN \n\n");
+    printf("Username :");
+    scanf(" %[^\n]s", &datauser.uname2);
+    printf("Password :");
+
+    while ((datauser.pw2 = (char)_getch()) != '\r')
+    {
+        if (datauser.pw2 == 8)
+        {
+            datauser.password2[j] = '\0';
+            printf(" ");
+            j--;
+            y--;
+        }
+        else
+        {
+            datauser.password2[j] = datauser.pw2;
+            printf("*");
+            y++;
+            j++;
+        }
+    }
+
+    printf("\n");
+
+    if ((strcmp(datauser.uname1, datauser.uname2) == 0) && (strcmp(datauser.password1, datauser.password2) == 0) && (coba >= 0))
+    {
         system("cls");
-        gotoxy(0,0);
-        printf("==================");
-        gotoxy(0,1);
-        printf("REGISTRASI & LOGIN");
-        gotoxy(0,2);
-        printf("==================");
-        gotoxy(5,3);
-        printf("1. Registrasi");
-        gotoxy(5,4);
-        printf("2. Login");
-        gotoxy(0,5);
-        printf("==================");
-        gotoxy(0, pos);
-        printf("==>");
-        int key = getch();
-        if (key == 72){
-            pos--;
-        }
-        else if (key == 80){
-            pos++;
-        }
-        else if (key == 13){
-            return pos - 2;
-        }
-        if (pos == 5){
-            pos = 3;
-        }
-        else if (pos == 2){
-            pos = 4;
-        }
+        printf("\nlogin berhasil");
+        getch();
+        system("cls");
+        // menuBANK();
     }
-}
-
-int menu_1_1(){ // menu registrasi
-    char key, id[100];
-    system("cls");
-    gotoxy(0,0);
-    printf("============================");
-    gotoxy(0,1);
-    printf("         REGISTRASI");
-    gotoxy(0,2);
-    printf("============================");
-    gotoxy(0,3);
-    printf("masukkan id       : ");
-    scanf("%s", &id);
-    strcpy(data_login[idx_data_login].nama,id);
-    for (int i = 0; i < idx_data_login; i++){
-        if (strcmp(data_login[i].nama, id) == 0){
+    else if ((strcmp(datauser.uname1, datauser.uname2) != 0 || strcmp(datauser.password1, datauser.password2) != 0) && coba >= 0)
+    {
+        if (coba != 0)
+        {
+            printf("\nPassword atau Username salah!\nAnda masih punya %d kesempatan", coba);
+            coba--;
+            getch();
             system("cls");
-            printf("Id Telah Digunakan");
-            Sleep(1000);
-            return menu_1_1();
+            login();
+        }
+        else
+        {
+            printf("\nMaaf, kesempatan login anda sudah habis");
+            getch();
         }
     }
-    printf("masukkan password : ");
-    get_password(&data_login[idx_data_login]);
-    idx_data_login ++;
-    system("cls");
-    printf("Registrasi Berhasil");
-    Sleep(1000);
-}
-
-int menu_1_2(){ // menu login
-    struct data_login login;
-    system("cls");
-    gotoxy(0,0);
-    printf("============================");
-    gotoxy(0,1);
-    printf("           LOGIN");
-    gotoxy(0,2);
-    printf("============================");
-    gotoxy(0,3);
-    printf("masukkan id       : ");
-    scanf("%s", &login.nama);
-    gotoxy(0,4);
-    printf("masukkan password : ");
-    get_password(&login);
-    system("cls");
-    for (int i = 0; i < idx_data_login; i++){
-        if(strcmp(login.nama, data_login[i].nama) == 0 && strcmp(login.password, data_login[i].password) == 0){
-            printf("Berhasil Login");
-            Sleep(1000);
-            login_cond = 1;
-            return 0;
-        }
-    }
-    if (coba){
-        printf("Maaf, id atau password salah!\nAnda masih punya %i kali kesempatan", coba);
-        coba--;
-        Sleep(1000);
-        return menu_1_2();
-    }
-    else{
-        printf("Maaf, kesempatan login anda telah habis.", coba);
-        Sleep(1000);
-        return system("exit");
+    else
+    {
+        printf("");
     }
 }
-// end bagian 1
 
+int main()
+{
 
-// tools
-void gotoxy(int x, int y){
-    COORD coor;
-    coor.X = x;
-    coor.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coor);
-}
-
-void get_password(struct data_login *target){
-    int key;
-    int i = 0;
-    char pass[8];
-    while ((key = getch()) != 13){
-        if (i < 0){
-            i = 0;
-        }
-        if (key == 8){
-            if (i == 0){
-                continue;
-            }
-            else{
-                printf("\b \b");
-                i--;
-                continue;
-            }
-        }
-        if (i == 8){
-            continue;
-        }
-        printf("*");
-        pass[i] = key;
-        i++;
-    }
-    pass[i] = '\0';
-    strcpy(target->password, pass);
+    system("COLOR f1");
+    regis();
 }
